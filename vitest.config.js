01fc019fef20c70d4190116c8d8377eed4c325d7
Andrii Yasnohorodskyi <yasnogorodskiy@gmail.com>
2023-06-03 19:41:57 +0100

    create base

--- vitest.config.js
+++ vitest.config.js
@@ -7,12 +7,14 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
+      setupFiles: ['./tests/setup.js'],
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       transformMode: {
-        web: [/\.[jt]sx$/],
-      },
+        web: [/\.[jt]sx$/]
+      }
     }
   })
 )
